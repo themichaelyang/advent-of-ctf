5c843ceec9fb6031af68a2540ffdc01abfb2d135
Michael Yang <themichaelyang@gmail.com>
2025-12-18 16:06:44 -0800

    clean up 2025 day 3

--- 3/3.rb
+++ 3/3.rb
@@ -1,74 +1,36 @@
 require "resolv"
 
+# References:
+# - https://www.cloudflare.com/learning/dns/dns-records/dns-spf-record/
+# - https://www.cloudflare.com/learning/dns/dns-records/dns-mx-record/
+# - https://www.cloudflare.com/learning/dns/dns-records/dns-a-record/
+# - https://mxtoolbox.com/dmarc/spf/what-is-an-spf-record/
+# - https://en.wikipedia.org/wiki/Sender_Policy_Framework
+# - https://www.cloudflare.com/learning/dns/dns-records/dns-dkim-record/
+
 # Overview: https://www.cloudflare.com/learning/dns/dns-records/
+
 # SRV, CAA, HTTPS, SVCB missing in the Resolv docs! Submit a PR
 # Resolv::DNS::Resource::IN.constants.map(&:to_s) - types
 RESOURCE_TYPES = [
   "A",
   "AAAA",
   "ANY",
-  # "AFSDB",
-  # "APL",
   "CAA",
-  # "CDNSKEY",
-  # "CDS",
-  # "CERT",
   "CNAME",
-  # "CSYNC",
-  # "DHCID",
-  # "DLV",
-  # "DNAME",
-  # "DNSKEY",
-  # "DS",
-  # "EUI48",
-  # "EUI64",
   "HINFO",
-  # "HIP",
   "HTTPS",
-  # "IPSECKEY",
-  # "KEY",
-  # "KX",
   "LOC",
   "MX",
   "MINFO",
-  # "NAPTR",
   "NS",
-  # "NSEC",
-  # "NSEC3",
-  # "NSEC3PARAM",
-  # "OPENPGPKEY",
   "PTR",
-  # "RP",
-  # "RRSIG",
-  # "SIG",
-  # "SMIMEA",
   "SOA",
   "SRV",
-  # "SSHFP",
   "SVCB",
-  # "TLSA",
   "TXT",
   "WKS"
-  # "URI",
-  # "ZONEMD",
 ]
-
-# NS, CNAME, SOA, PTR, HINFO, MINFO, MX, TXT, LOC, ANY, CAA
-#
-# RESOURCE_TYPES = %w[
-#   A
-#   AAAA
-#   ANY
-#   CNAME
-#   HINFO
-#   MINFO
-#   MX
-#   NS
-#   PTR
-#   SOA
-#   TXT
-#   WKS
-# ]
 
 def dns_records(domain)
   Resolv::DNS.open do |dns|
@@ -102,15 +64,88 @@ hash = [
   [domain, dns_records(domain)]
 end
 
-# TXT: v=spf1 include:_spf.krampus.csd.lol -all
-# MX: mail.krampus.csd.lol.
-
-# https://www.cloudflare.com/learning/dns/dns-records/dns-spf-record/
-# https://www.cloudflare.com/learning/dns/dns-records/dns-mx-record/
-# https://www.cloudflare.com/learning/dns/dns-records/dns-a-record/
-# https://mxtoolbox.com/dmarc/spf/what-is-an-spf-record/
-# https://en.wikipedia.org/wiki/Sender_Policy_Framework
-# https://www.cloudflare.com/learning/dns/dns-records/dns-dkim-record/
+# {"krampus.csd.lol" =>
+#   {"MX" =>
+#     [#<Resolv::DNS::Resource::Type15_Class1:0x00000001044b6338
+#       @exchange=#<Resolv::DNS::Name: mail.krampus.csd.lol.>,
+#       @preference=10,
+#       @ttl=300>],
+#    "TXT" =>
+#     [#<Resolv::DNS::Resource::Type16_Class1:0x0000000104648570
+#       @strings=["v=spf1 include:_spf.krampus.csd.lol -all"],
+#       @ttl=300>]},
+#  "mail.krampus.csd.lol" =>
+#   {"A" =>
+#     [#<Resolv::DNS::Resource::IN::A:0x00000001046436d8
+#       @address=#<Resolv::IPv4 127.0.0.1>,
+#       @ttl=300>]},
+#  "_spf.krampus.csd.lol" =>
+#   {"TXT" =>
+#     [#<Resolv::DNS::Resource::Type16_Class1:0x000000010444bee8
+#       @strings=["v=spf1 ip4:203.0.113.0/24 ~all"],
+#       @ttl=300>]},
+#  "_dmarc.krampus.csd.lol" =>
+#   {"TXT" =>
+#     [#<Resolv::DNS::Resource::Type16_Class1:0x000000010464e5b0
+#       @strings=
+#        ["v=DMARC1; p=reject; rua=mailto:dmarc@krampus.csd.lol; ruf=mailto:forensics@ops.krampus.csd.lol; fo=1; adkim=s; aspf=s"],
+#       @ttl=300>]},
+#  "ops.krampus.csd.lol" =>
+#   {"TXT" =>
+#     [#<Resolv::DNS::Resource::Type16_Class1:0x00000001046e96c8
+#       @strings=
+#        ["internal-services: _ldap._tcp.krampus.csd.lol _kerberos._tcp.krampus.csd.lol _metrics._tcp.krampus.csd.lol"],
+#       @ttl=300>]},
+#  "_ldap._tcp.krampus.csd.lol" =>
+#   {"SRV" =>
+#     [#<Resolv::DNS::Resource::IN::SRV:0x00000001042da910
+#       @port=389,
+#       @priority=0,
+#       @target=#<Resolv::DNS::Name: dc01.krampus.csd.lol.>,
+#       @ttl=300,
+#       @weight=0>]},
+#  "_kerberos._tcp.krampus.csd.lol" =>
+#   {"SRV" =>
+#     [#<Resolv::DNS::Resource::IN::SRV:0x00000001046cb100
+#       @port=88,
+#       @priority=0,
+#       @target=#<Resolv::DNS::Name: dc01.krampus.csd.lol.>,
+#       @ttl=300,
+#       @weight=0>]},
+#  "_metrics._tcp.krampus.csd.lol" =>
+#   {"SRV" =>
+#     [#<Resolv::DNS::Resource::IN::SRV:0x0000000104380f18
+#       @port=443,
+#       @priority=0,
+#       @target=#<Resolv::DNS::Name: beacon.krampus.csd.lol.>,
+#       @ttl=300,
+#       @weight=0>]},
+#  "beacon.krampus.csd.lol" =>
+#   {"A" =>
+#     [#<Resolv::DNS::Resource::IN::A:0x000000010466fc10
+#       @address=#<Resolv::IPv4 203.0.113.2>,
+#       @ttl=300>],
+#    "TXT" =>
+#     [#<Resolv::DNS::Resource::Type16_Class1:0x00000001042ec958
+#       @strings=["config=ZXhmaWwua3JhbXB1cy5jc2QubG9s=="],
+#       @ttl=300>]},
+#  "dc01.krampus.csd.lol" =>
+#   {"A" =>
+#     [#<Resolv::DNS::Resource::IN::A:0x000000010464b360
+#       @address=#<Resolv::IPv4 203.0.113.1>,
+#       @ttl=300>]},
+#  "exfil.krampus.csd.lol" =>
+#   {"TXT" =>
+#     [#<Resolv::DNS::Resource::Type16_Class1:0x0000000104666340
+#       @strings=["status=active; auth=dkim; selector=syndicate"],
+#       @ttl=300>]},
+#  "syndicate._domainkey.krampus.csd.lol" =>
+#   {"TXT" =>
+#     [#<Resolv::DNS::Resource::Type16_Class1:0x0000000104641ab8
+#       @strings=["v=DKIM1; k=rsa; p=Y3Nke2RuNV9tMTlIVF9CM19LMU5ENF9XME5LeX0="],
+#       @ttl=300>]}}
+# "exfil.krampus.csd.lol"
+# "csd{dn5_m19HT_B3_K1ND4_W0NKy}
 
 pp hash
 
